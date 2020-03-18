41a11530b38f0264fe5c795df1b6685319389409
adosix <andrejjezik@gmail.com>
2020-03-18 23:17:09 +0100

    interpret (iterating throught instructions)

--- interpret.py
+++ interpret.py
@@ -4,32 +4,41 @@ Interpreter of XML representation of IPPcode20.
 Author: Andrej Ježík
 School login: xjezik03
 """
-
-from xml.dom.minidom import parse, parseString
 import sys
+import xml.etree.ElementTree as ET
 
 # --------  main definition  --------
 def main():
+     #----ARGUMENT proccessig
      file_path = processArguments()
-     print(file_path)
-     # --- Open input file ---
+     #----XML FILE parsing
+     xml_tree = parse_file(file_path)
+     #----GENERAL SYNTAX OF ROOT checked 
+     root = check_syntax(xml_tree)
+     #----INSTRUCTION proccessing
+     process_instructions(root)
+
+def process_instructions(root):
+     for inst in root:
+          print(inst)
+
+def check_syntax(xml_tree):
+     root = xml_tree.getroot()
+     if(root.tag != "program") or (root.attrib['language']  !=  "IPPcode20"):
+          error(32)
+     for atr in root.attrib:
+          if (atr != "language") and (atr != "name") and (atr != "description"):
+               error(32)  
+     return root   
+
+def parse_file(file_path):
      try:
-          doc = parse(file=file_path)
+          tree = ET.parse(file_path)
+          return tree
      except IOError:
           error(11)
-     except ET.ParseError:
-          error(31)
      except:
-          error(99)
-
-     # --- Check root node ---
-     root = tree.getroot()
-
-     # --- Process instructions ---
-     Interpret.loadInstructions(root)
-
-     # --- Successful end ---
-     sys.exit(0)
+          error(31)
 
 def processArguments():
      # check correct number of arguments
@@ -48,18 +57,31 @@ def processArguments():
      --input=file  -> file with input XML files 
           """ 
           print(h)
+          sys.exit(0)
      else:
-          return "bullshit"
+          error(10)
 
 def error(err_val):
      switch = {
           10: "Wrong combination of parameters",
           11: "Error while opening input file/s",
           12: "Error while opening output file/s",
+          31: "Wrong XML format fo input file/s",
+          32: "unexpected structure of XML file, lexical of syntax error",
           99: "internal error"
      }
-     print(sys.stderr, switch.get(err_val, "Invalid error code"))
-     exit(err_val)
+     print(sys.stderr,str(err_val) + ": " + switch.get(err_val,": Invalid error code"))
+     sys.exit(err_val)
 
+#---- global variables
 
+instr_total = 0
+local_frame = None
+temp_frame = None
+stack = []
+frame_stack = []
+call_stack = []
+order = 1
+labels = {}
+global_frame = {}
 main()
